267491a3eee45c7c655a2a599fa0c338e2cafeea
Vladislav Boyarkin <boyarkinvladislavv@gmail.com>
2021-11-23 21:25:19 +0300

    Create vertical bar chart component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
 <template>
   <div id="app">
-
+    <VerticalBarChart v-bind:values="values1" />
+    <VerticalBarChart
+      v-bind:values="values2"
+      v-bind:width="width2"
+      v-bind:scale="scale2"
+    />
   </div>
 </template>
 
 <script>
+import VerticalBarChart from "./components/VerticalBarChart.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      values1: [50, 100, 150, 70, 55],
+      values2: [70, 150, 44, 71, 55],
+      scale2: 2,
+      width2: 6,
+    };
+  },
   components: {
-  }
-}
+    VerticalBarChart,
+  },
+};
 </script>
 
 <style>
-
 </style>
---
+++ src/components/VerticalBarChart.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="barchart-container">
+    <div
+      v-for="(value, i) of values"
+      class="barchart-container__bar"
+      v-bind:key="i"
+      :style="barStyle(value)"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  /**
+   * Vertical bar chart
+    */
+  props: {
+    /**
+     * 
+     */
+    values: {
+      type: Array,
+    },
+    width: {
+      type: Number,
+      default: 25,
+    },
+    scale: {
+      type: Number,
+      default: 1,
+    },
+  },
+  methods: {
+    barStyle(value) {
+      return `
+        height: ${value*this.scale}px;
+        width: ${this.width}px
+      `;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.barchart-container {
+  display: flex;
+  align-items: flex-end;
+  margin: 5px;
+}
+.barchart-container__bar {
+  margin: 0px 5px;
+  background-color: rgb(47, 118, 180);
+}
+</style>
